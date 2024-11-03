7f82703d0687acf1a0f81e6582c49133f0d4c40d
Mits16 <mits1388@gmail.com>
2024-11-03 12:58:12 +0300

    add class login base

---
+++ src/test/java/by/pizzatempo/BaseTest.java
@@ -0,0 +1,25 @@
+package by.pizzatempo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        driver.manage().window().maximize();
+        driver.get("https://www.pizzatempo.by/");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
