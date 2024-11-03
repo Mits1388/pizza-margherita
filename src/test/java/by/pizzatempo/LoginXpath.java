1542bb4c65b462c7e417bdbe60b62ec53088caf9
Mits16 <mits1388@gmail.com>
2024-11-03 12:57:18 +0300

    add class login xpath

---
+++ src/test/java/by/pizzatempo/LoginXpath.java
@@ -0,0 +1,10 @@
+package by.pizzatempo;
+
+public class LoginXpath {
+    public static final String BUTTON_PIZZA_XPATH = "/html/body/div[1]/div[3]/ul/li[1]/a";
+    public static final String BUTTON_SIZE_31_XPATH = "/html/body/div[1]/div[5]/div[2]/div[2]/div[1]/div[10]/div[3]/div/div[2]/div[2]/div[3]";
+    public static final String BUTTON_ORDER_ONE_XPATH = "/html/body/div[1]/div[5]/div[2]/div[2]/div[1]/div[10]/div[3]/div/div[2]/div[3]/div[3]/button/span/span";
+    public static final String BUTTON_ORDER_TWO_XPATH = "/html/body/div[4]/div[2]/div[2]/div[2]/div[1]/div[3]/div[4]/button";
+    public static final String BUTTON_BASKET_XPATH = "/html/body/div[1]/div[1]/div[2]/div[2]/div/div/div[1]/div[1]/a";
+    public static final String TEXT_ORDER_XPATH = "/html/body/div[1]/div[5]/div[2]/div[2]/div[1]/div[1]/table/tbody/tr[1]/td[2]/div[1]";
+}
